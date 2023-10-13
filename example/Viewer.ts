f9078f964a660c62915497dea538cb00d52e4b0b
André "MG" Wisén <andre.wisen@gmail.com>
2023-10-13 08:29:04 +0200

    Add `version`

--- example/Viewer.ts
+++ example/Viewer.ts
@@ -81,7 +81,7 @@ export default class Viewer {
         this._stats = new Stats();
         document.body.appendChild(this._stats.dom);
 
-        this._gui = new GUI();
+        this._gui = new GUI().title(`three-tunnel v${VERSION}`);
 
         this._clock = new THREE.Clock();
 
