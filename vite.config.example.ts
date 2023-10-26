dd43e32cca26711f1b8f2ac34b5dd6ca5459df67
André "MG" Wisén <andre.wisen@gmail.com>
2023-10-26 20:35:13 +0200

    Fix `modifyURLPrefix`

--- vite.config.example.ts
+++ vite.config.example.ts
@@ -36,7 +36,7 @@ export default defineConfig({
             },
             workbox: {
                 modifyURLPrefix: {
-                    '': './../',
+                    '': './',
                 },
             },
         }),
