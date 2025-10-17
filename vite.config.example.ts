3d10865b3820f4d9506f8cc393798fc22583fa54
André Wisén <andre.wisen@tikab.com>
2025-10-17 08:07:45 +0200

    bump

--- vite.config.example.ts
+++ vite.config.example.ts
@@ -1,53 +1,51 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { splitVendorChunkPlugin } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        splitVendorChunkPlugin(),
-        VitePWA({
-            base: './../',
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Tunnel Sketcher',
-                short_name: 'Tunnel Sketcher',
-                description: 'Tunnel and grouts sketcher',
-                start_url: './example/',
-                theme_color: '#ffffff',
-                background_color: '#ffffff',
-                display: 'standalone',
-                icons: [
-                    {
-                        src: './favicon/android-chrome-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        purpose: 'any maskable',
-                    },
-                    {
-                        src: './favicon/android-chrome-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any maskable',
-                    },
-                ],
-            },
-            workbox: {
-                modifyURLPrefix: {
-                    '': './',
-                },
-            },
-        }),
-    ],
-    base: './',
-    build: {
-        outDir: './dist/examples',
-        rollupOptions: {
-            input: {
-                example: resolve(__dirname, 'example/index.html'),
-            },
-        },
-    },
+	plugins: [
+		react(),
+		VitePWA({
+			base: "./../",
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Tunnel Sketcher",
+				short_name: "Tunnel Sketcher",
+				description: "Tunnel and grouts sketcher",
+				start_url: "./example/",
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				icons: [
+					{
+						src: "./favicon/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+					{
+						src: "./favicon/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				modifyURLPrefix: {
+					"": "./",
+				},
+			},
+		}),
+	],
+	base: "./",
+	build: {
+		outDir: "./dist/examples",
+		rollupOptions: {
+			input: {
+				example: resolve(__dirname, "example/index.html"),
+			},
+		},
+	},
 });
